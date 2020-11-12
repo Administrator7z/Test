6d2f13021e9d5a3747be11a9e4569c08078aa79d
Administrator7z <beyond7z@outlook.com>
2020-11-12 08:49:40 +0800

    1

---
+++ src/Demo03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Demo03{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" ‰»Î");
+		int num = sc.nextInt();
+		if(num % 4 == 0 && num % 100 != 0) {
+			System.out.println(num + "»Ú");
+		}
+		else {
+			if(num % 400 == 0) { 
+				System.out.println(num + "»Ú");
+			}
+			else {
+				System.out.println(num +"≤ª «");
+			}
+			
+		}
+	}
+}
