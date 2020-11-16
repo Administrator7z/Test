61b2b648ecf90f04d9dcfadfcada2073b2ee2871
Administrator7z <beyond7z@outlook.com>
2020-11-16 17:28:06 +0800

    1

---
+++ src/com/vipzou/javasetest/dy01_dy14/Demo07Test.java
@@ -0,0 +1,9 @@
+package com.vipzou.javasetest.dy01_dy14;
+
+public class Demo07Test {
+    public static void main(String[] args) {
+        Demo07 aa = new Demo07();
+        System.out.println(aa.toString());
+    }
+}
+
